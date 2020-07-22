5adeddbdf9b16ce3251810b9ab45438f15370a16
Andy Huynh <andy.huynh312@gmail.com>
2020-07-21 21:29:50 -0700

    Changes needed for Cypress

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -4,6 +4,7 @@ require('express-async-errors');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 const blogRouter = require('./controllers/blog');
 const userRouter = require('./controllers/user');
 const loginRouter = require('./controllers/login');
@@ -17,10 +18,15 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('combined'));
 app.use(middleware.tokenExtractor);
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', userRouter);
 app.use('/api/login', loginRouter);
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing');
+  app.use('/api/testing', testingRouter);
+}
 app.use(middleware.errorHandler);
 
 module.exports = app;
